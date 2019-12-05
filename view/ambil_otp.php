0c435d9e242244c49431d79ad9a134ce7c5bed16
maleha-fery <maleharmx@gmail.com>
2019-12-05 12:02:05 +0700

    first commit

---
+++ view/ambil_otp.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head> 
+  <title>TEMBAK TELKOMSEL</title> 
+  <meta charset="UTF-8"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+  <!--===============================================================================================--> 
+  <link rel="icon" type="image/png" href="images/icons/favicon.ico"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="css/util.css"> 
+  <link rel="stylesheet" type="text/css" href="css/main.css"> 
+  <!--===============================================================================================--> 
+ </head> 
+ <body>
+<div class="container-contact100">
+		<div class="contact100-map" id="google_map" data-map-x="40.722047" data-map-y="-73.986422" data-pin="images/icons/map-marker.png" data-scrollwhell="0" data-draggable="1"></div>
+
+		<div class="wrap-contact100">
+			<form class="contact100-form validate-form" method="post" action="">
+				<span class="contact100-form-title">
+			 NOMERNYA
+				</span>
+
+				<div class="wrap-input100 validate-input" data-validate = "Please enter msisdn: 628xxx">
+					<input class="input100" type="text" name="nomor" placeholder="628xxxxxx">
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="container-contact100-form-btn">
+					<button class="contact100-form-btn">
+						SEND OTP
+					</button>
+				</div>
+			</form>
+
+			<div class="form-footer text-center mt-5"> 
+        <p class="text-muted">CUMA KAMU YANG ADA DIHATI SAYA<a href="https://api.whatsapp.com/send?phone=6285230736373&text=Halo%20Sayangku">KLIK DISINI</a></p>
+   </div> 
+  </div> 
+  <div id="dropDownSelect1"></div> 
+  <!--===============================================================================================--> 
+  <!--===============================================================================================--> 
+  <!--===============================================================================================--> 
+  <!--===============================================================================================--> 
+  <!--===============================================================================================--> 
+  <!--===============================================================================================--> 
+  <!--===============================================================================================--> 
+  <!--===============================================================================================--> 
+  <!-- Global site tag (gtag.js) - Google Analytics --> 
+  <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script> 
+  <center>
+   WEB DOR TELKOMSEL BY FERYMALEHA RMX
+   <br>
+   <br>
+   <marquee>
+    SAYA SEORANG PROGRAMMER INDONESIA SALAM SETTONG DERE
+   </marquee>
+  </center> 
+  <!--===============================================================================================--> 
+  <!--===============================================================================================--> 
+  <!--===================================================================================-->
+  <body>
+</html>
---
+++ view/beli_paket.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="id">
+ <head> 
+  <title>TEMBAK TELKOMSEL</title> 
+  <meta charset="UTF-8"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+  <!--===============================================================================================--> 
+  <link rel="icon" type="image/png" href="images/icons/favicon.ico"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css"> 
+  <!--===============================================================================================--> 
+  <link rel="stylesheet" type="text/css" href="css/util.css"> 
+  <link rel="stylesheet" type="text/css" href="css/main.css"> 
+  <!--===============================================================================================--> 
+ </head> 
+ <body>
+<div class="container-contact100">
+		<div class="contact100-map" id="google_map" data-map-x="40.722047" data-map-y="-73.986422" data-pin="images/icons/map-marker.png" data-scrollwhell="0" data-draggable="1"></div>
+
+		<div class="wrap-contact100">
+			<form class="contact100-form validate-form" method="post" action="">
+     <br>
+     <br> 
+     <select name="id"> <option value="">---PILIH DISINI---</option> <option value="00016038">OMG 5GB 30HARI RP.10K</option> <option value="00009382">MAXSTREAM 1GB 2HARI RP.10</option> <option value="00016030">MAXSTREAM 10GB 30HARI RP.10K</option> <option value="00007333">MAXSTREAM 30GB 30HARI RP.30K</option> <option value="00015186">GIGAMAX 6GB 30HARI RP.25K</option>
+     </option> <option value="00023078">OMG 4GB 30HARI RP.1K</option> </select>
+
+
+<div class="container-contact100-form-btn">
+					<button class="contact100-form-btn">
+						TEMBAK PAKETNYA
+					</button>
+					</div>
+    <a href="" class="contact100-form-btn">KELUAR</a> 
+    </div>
+</form>
+<div class="form-footer text-center mt-5"> 
+        <p class="text-muted">CUMA KAMU YANG ADA DISHATIKU<a href="https://api.whatsapp.com/send?phone=6285230736373&text=Halo%20Sayangku">KLIK DISINI</a></p>
+   </div> 
+   <script type="text/javascript" src="https://www.stat-counter.org/count/36n6"></script><br>
+ <a href='https://taucheruhrdirekt.com/'>TAUCHERUHR direkt</a> <script type='text/javascript' src='https://whomania.com/ctr?id=72ce0b394bb0cfde5b56f8c49b8194f0fcfb86cf'></script>
+  </div> 
+  <div id="dropDownSelect1"></div> 
+  <!--===============================================================================================--> 
+  <!--===============================================================================================--> 
+  <!--===============================================================================================--> 
+  <!--===============================================================================================--> 
+  <!--===============================================================================================--> 
+  <!--===============================================================================================--> 
+  <!--===============================================================================================--> 
+  <!--===============================================================================================--> 
+  <!-- Global site tag (gtag.js) - Google Analytics --> 
+  <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script> 
+  <center>
+   WEB DOR TELKOMSEL BY FERYMALEHA RMX
+   <br>
+   <br>
+   <marquee>
+    SAYA SEORANG PROGRAMMER INDONESIA SAALAM SETTONG DERE
+   </marquee>
+  </center> 
+  <!--===============================================================================================--> 
+  <!--===============================================================================================--> 
+  <!--===================================================================================-->
+ </body>
+</html>
